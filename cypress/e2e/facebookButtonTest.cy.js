9eca071c535c4799cf2da4deec04e8cdbb763269
Anna <annaurbaniak18@gmail.com>
2024-06-25 13:56:50 +0200

    Changed facebook test and add new file.yml.

--- cypress/e2e/facebookButtonTest.cy.js
+++ cypress/e2e/facebookButtonTest.cy.js
@@ -4,7 +4,9 @@ describe("Facebook button test", () => {
 
     cy.get(".facebook").scrollIntoView().trigger("focus");
 
-    cy.get(".facebook a._blank").invoke("removeAttr", "target").click();
+    cy.get(".facebook a._blank")
+      .invoke("removeAttr", "target")
+      .click({ timeout: 8000 });
 
     cy.origin("https://www.facebook.com", () => {
       cy.url().should("include", "//www.facebook.com/prestashop");
