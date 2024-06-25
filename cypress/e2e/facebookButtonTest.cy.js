f8ed57d27214764fa1cb24f7c952f8a015bec409
Anna <annaurbaniak18@gmail.com>
2024-06-25 14:08:45 +0200

    Added waiting time.

--- cypress/e2e/facebookButtonTest.cy.js
+++ cypress/e2e/facebookButtonTest.cy.js
@@ -4,11 +4,10 @@ describe("Facebook button test", () => {
 
     cy.get(".facebook").scrollIntoView().trigger("focus");
 
-    cy.get(".facebook a._blank")
-      .invoke("removeAttr", "target")
-      .click({ timeout: 8000 });
+    cy.get(".facebook a._blank").invoke("removeAttr", "target").click();
 
     cy.origin("https://www.facebook.com", () => {
+      cy.wait(5000);
       cy.url().should("include", "//www.facebook.com/prestashop");
     });
   });
